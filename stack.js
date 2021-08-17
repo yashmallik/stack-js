39ca02f326fa43b8365cbcdb45e9d2558fdc0564
Yash Mallik <yashmallik12@gmail.com>
2021-08-17 22:03:47 +0530

    last

--- stack.js
+++ stack.js
@@ -1,19 +1,19 @@
 stack =[]
-changes(["push 2","push 4 ","pop","push 5","pop","push 8","push 5","pop","push 9", "add 2 3","add 2 4"])
+comand = ["push 2","push 4","pop","push 5","pop","push 8","push 5","pop","push 9", "add 2 3","add 2 4"]
+changes(comand)
 
 function changes(arr){
-    for(i in arr){
-       let eachElement = i.split(" ");
+    for(let i = 0; i < arr.length;i++){
+       let eachElement = arr[i].split(" ");
        if (eachElement[0] =="push")  stack.push(eachElement[1]);
        else if(eachElement[0] =="pop") stack.pop(); 
-       else if(eachElement[0] =="add") add();
-       console.log(eachElement)
+       else if(eachElement[0] =="add") add(stack, eachElement)
+        if(stack.length == 0) console.log("stack is empty");
     }
-    console.log(stack)
+    return stack
 }
-function add(){
-    for(let j=1; j<eachElement[1]; j++){
-        stack[i] += eachElement[2];
-
+function add(stack, eachElement) {
+    for(let j=1; j<=eachElement[1]; j++){
+        stack[stack.length-j] = parseInt(stack[stack.length-j]) + parseInt(eachElement[2]);
     }
 }
